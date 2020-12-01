24cd497201834c32a0e78b6355168eb85b47ed87
James Davidson <james.la.davidson@gmail.com>
2020-12-01 12:24:40 -0500

    added css and bootstrap styling and dark mode toggle switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 import './App.css';
+import './ToggleSwitch.scss';
+import ToggleSwitch from './ToggleSwitch.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      movies: []
+      movies: [],
+      getClassNameApp: "App",
+      getClassNameCard: "",
     }
+    this.darkmodeSwitch = this.darkmodeSwitch.bind(this);
   }
 
   componentDidMount() {
@@ -16,31 +23,46 @@ class App extends React.Component {
   }
 
   loadMovies() {
-    // go grab (aka get) all the data from some url
     const url = "https://api.themoviedb.org/3/discover/movie?api_key=b6fbc7f3f313bd395902af464ef47262&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
     axios.get(url)
       .then(response =>
       {
-        // then put just the movies (not everything) into the state
         this.setState({movies: response.data.results});
-      });
+      }
+    );
+  }
 
+  darkModeSwitch() {
+    if (this.state.getClassNameApp==="App") {
+      this.setState({getClassNameApp: "App DarkMode", getClassNameCard: "cardDarkMode"});
+    }
+    else {
+      this.setState({getClassNameApp: "App", getClassNameCard: ""});
+    }
   }
 
   render() {
-    // let movies = [];
-    // for(let i = 0; i < this.state.movies.length; i++)
-    // {
-    //   movies.push(<h2>{this.state.movies[i].title}</h2>)
-    // }
-
     return (
-      <div className="App">
-        <h1>Check out these movies!!!!!!</h1>
-        {this.state.movies.map(movie =>
-          {
-            return <h2>{movie.title} ({movie.release_date})</h2>;
-          })}
+      <div className={this.state.getClassNameApp}>
+        <h1>Check out this year's Oscar bait, y'all:</h1>
+        <p>Search: <input></input></p>
+        <ToggleSwitch Name="DarkMode" Checked="false" changeSwitch={this.darkModeSwitch}/>
+          <div className="MovieGrid">
+            {this.state.movies.map(movie =>
+              {
+                let movieURL = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+                return <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={movieURL} />
+                    <Card.Body className={this.state.getClassNameCard}>
+                      <Card.Title>{movie.title} ({movie.release_date})</Card.Title>
+                      <Card.Text>
+                        {movie.overview}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+              })
+            }
+        </div>
       </div>
     );
   }
